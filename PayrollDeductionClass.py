109d4d4da4ca9720c493dbe8ee29adedda3238a8
John <John_Drexelius1@baylor.edu>
2022-09-27 14:53:42 -0500

    Initial commit

---
+++ PayrollDeductionClass.py
@@ -0,0 +1,6 @@
+# Write a class that represents a payroll deduction transaction. 
+# Each instance should have the description, date, charge amount
+# and employee ID as attributes. The class’s __init__ method should accept 
+# an argument for each attribute. The class should have accessor methods for each attribute.
+# All attribute should be hidden.
+
